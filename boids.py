8a5b76d3dd008cac905650f7e1502c0e3b03a4ae
Francois Ni <41174986+nip403@users.noreply.github.com>
2021-12-28 12:22:56 +0800

    Update boids.py

--- boids.py
+++ boids.py
@@ -1,29 +1,15 @@
-from graphics import Window
-from ai import Boid, init_boids, manage_overlap
-import pygame
+from ai import Boid, init_boids
+from graphics import Demo
+
+# add option (sliders/keypresses) to modify sim constants
 
 def main():
-    s = [1200, 800]
-    boids = init_boids(10, s)
-    win = Window(*s)
+    dimensions = [1400, 1000]
+    boids = init_boids(100, dimensions)
     
-    while True:
-        win.clock.tick(60)
-        
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-                
-        for b in boids:
-            b.apply_rules(boids, s)
-            b.init_next(s)
-            
-        for b in boids:
-            next(b)
-                
-        win.update(boids)
-    
+    demo = Demo(dimensions)
+    demo.update_boids(boids)
+    demo.sim()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
